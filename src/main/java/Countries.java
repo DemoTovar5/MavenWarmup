c0772f9f7343205ef3ca3b0c7eb4266241f8e171
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-22 10:05:37 -0500

    Countries

--- src/main/java/Countries.java
+++ src/main/java/Countries.java
@@ -1,10 +1,49 @@
 public class Countries {
 
-    // fields go here
-
     public static void main(String[] args) {
 
+        Countries Demo = new Countries("USA", 500,"English");
+
+        System.out.println("I live in the " + Demo.Country + " with " + Demo.Population + " people" + " and we speak " + Demo.Language);
     }
+
+
+    // fields go here
+    private String Country;
+    private double Population;
+    private String Language;
+
+    public Countries(String country, double population, String language) {
+        Country = country;
+        Population = population;
+        Language = language;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public double getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(double population) {
+        Population = population;
+    }
+
+    public String getLanguage() {
+        return Language;
+    }
+
+    public void setLanguage(String language) {
+        Language = language;
+    }
+
+
 
 
 }
